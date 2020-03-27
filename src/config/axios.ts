c24a03629e6b69fcb32de0ae31bc32f5ae552eed
wuyiyan <308111394@qq.com>
2020-03-27 14:29:35 +0800

    配置axios

---
+++ src/config/axios.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+
+const appId = "sv2eTjKsNCV5BkjXfBRuMHgo"
+const appSecret = "CtvAYi3xZp9G12Ld2yMzQL8T"
+
+const instance = axios.create({
+    baseURL: 'https://gp-server.hunger-valley.com/',
+    // timeout: 1000,
+    headers: {
+        't-app-id' : appId,
+        't-app-secret' : appSecret
+    }
+});
+
+instance.interceptors.request.use(function (config) {
+    const xToken = localStorage.getItem( 'x-token')
+    if(xToken) {
+        config.headers['Authorization'] = `Bearer ${xToken}`
+    }
+    return config;
+}, function (error) {
+    console.error(error)
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use(function (response) {
+    const token = response.headers['x-token']
+    if(token) {
+        localStorage.setItem('x-token',token)
+    }
+    return response;
+}, function (error) {
+    
+    return Promise.reject(error);
+});
+
+export default instance
