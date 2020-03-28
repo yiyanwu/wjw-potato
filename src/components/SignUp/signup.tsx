1b0ebf79d6cb41a239de95ccecf77722a9445b38
wuyiyan <308111394@qq.com>
2020-03-28 18:39:15 +0800

    初步完成注册页面

--- src/components/SignUp/signup.tsx
+++ src/components/SignUp/signup.tsx
@@ -1,14 +1,77 @@
 import * as React from 'react'
+import { Input, Button} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import axios from '../../config/axios'
 
-class Component extends React.Component {
+
+interface SignUpState {
+    account: string,
+    password: string,
+    passwordConfirmation: string
+}
+
+class SignUp extends React.Component<any, SignUpState> {
+    constructor(props: any){
+        super(props)
+        this.state = {
+            account: '',
+            password: '',
+            passwordConfirmation: ''
+        };
+    }
+
+    onChangeAccount = (e:any) =>{
+        this.setState( {account: e.target.value})
+    }
+
+    onChangePassword = (e: any) => {
+        this.setState({ password: e.target.value })
+    }
+
+    onChangePasswordConfirmation = (e: any) => {
+        this.setState({ passwordConfirmation: e.target.value })
+    }
+
+    submit = async() => {
+        const { account, password, passwordConfirmation } = this.state
+        try{
+            await axios.post('sign_up/user',{
+                account,
+                password,
+                password_confirmation: passwordConfirmation
+            })
+            console.log('成功')
+        }catch (e){
+            throw new Error(e)
+        }
+    }
 
     render() {
+        const { account,password,passwordConfirmation} = this.state
         return (
-            <div className="Component">
-                signup
+            <div className="SignUp">
+                <Input
+                    placeholder="请输入用户名"
+                    value= {account}
+                    onChange={this.onChangeAccount}
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                />
+                <Input.Password 
+                    placeholder="请输入密码"
+                    value= {password}
+                    onChange={this.onChangePassword}
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                />
+                <Input.Password
+                    placeholder="请确认密码"
+                    value={passwordConfirmation}
+                    onChange={this.onChangePasswordConfirmation}
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                />
+                <Button onClick={this.submit}>注册</Button>
             </div>
         )
     }
 }
 
-export default Component
+export default SignUp
