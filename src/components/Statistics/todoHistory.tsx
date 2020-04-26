763f76cdbb95df1285670cabcf38ae6759a49e27
wuyiyan <308111394@qq.com>
2020-04-26 16:50:33 +0800

    引入antd tabs分页组件

--- src/components/Statistics/todoHistory.tsx
+++ src/components/Statistics/todoHistory.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react'
-import {connect} from 'react-redux'
-import { format, parseISO} from 'date-fns'
+import { connect } from 'react-redux'
+import { format, parseISO } from 'date-fns'
+import { Tabs } from 'antd'
 import _ from 'lodash'
 
 interface todoHistoryProps {
-    todos:any[]
+    todos: any[]
 }
 
-const TodoItem = function (param:any) {
-    return(
+const { TabPane } = Tabs;
+
+const TodoItem = function (param: any) {
+    return (
         <div>
             <span>{format(parseISO(param.updated_at), 'H:mm')}</span>
             <span>{param.description}</span>
@@ -18,28 +21,28 @@ const TodoItem = function (param:any) {
 
 class TodoHistory extends React.Component<todoHistoryProps> {
 
-    get finishedTodos (){
+    get finishedTodos() {
         return this.props.todos.filter(t => t.completed && !t.deleted)
     }
 
-    get deletedTodos (){
+    get deletedTodos() {
         return this.props.todos.filter(t => t.deleted)
     }
 
-    get dailyFinishedTodos (){
+    get dailyFinishedTodos() {
         return _.groupBy(this.finishedTodos, (t: any) => {
             return format(parseISO(t.updated_at), 'yyyy-MM-d')
         })
     }
 
-    get dates (){
-        return Object.keys(this.dailyFinishedTodos).sort((a,b) => Date.parse(b) - Date.parse(a))
+    get dates() {
+        return Object.keys(this.dailyFinishedTodos).sort((a, b) => Date.parse(b) - Date.parse(a))
     }
 
-    render(){
+    render() {
         const todoList = this.dates.map(date => {
-            return(
-                <div key={date}> 
+            return (
+                <div key={date}>
                     <div>
                         <span>{date}</span>
                         <span>完成了{this.dailyFinishedTodos[date].length}个任务</span>
@@ -47,17 +50,24 @@ class TodoHistory extends React.Component<todoHistoryProps> {
                     <div>
                         {
                             this.dailyFinishedTodos[date].map(todo =>
-                                <TodoItem key={todo.id} {...todo}/>)
+                                <TodoItem key={todo.id} {...todo} />)
                         }
                     </div>
                 </div>
             )
         })
 
-        return(
-            <div className="TodoHistory" id="TodoHistory">
-                {todoList}
-            </div>
+        return (
+            <Tabs defaultActiveKey="1">
+                <TabPane tab="已完成的任务" key="1">
+                    <div className="TodoHistory" id="TodoHistory">
+                        {todoList}
+                    </div>
+                </TabPane>
+                <TabPane tab="已删除的任务" key="2">
+                    Content of Tab Pane 2
+                </TabPane>
+            </Tabs>
         )
     }
 }
