676f6615268b5c1d0b5ab5618c8889f44af85969
wuyiyan <308111394@qq.com>
2020-05-12 16:17:25 +0800

    Updates

---
+++ precache-manifest.8865c58dfa040420a07b5dc72200fcdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dfc30e1457893458d1bd379fea258ee",
+    "url": "/wjw-potato/index.html"
+  },
+  {
+    "revision": "727a0d644777c23261f8",
+    "url": "/wjw-potato/static/css/2.ac55bf83.chunk.css"
+  },
+  {
+    "revision": "4c1f6cd639b24d2701f0",
+    "url": "/wjw-potato/static/css/main.c9260b39.chunk.css"
+  },
+  {
+    "revision": "727a0d644777c23261f8",
+    "url": "/wjw-potato/static/js/2.d12f5299.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/wjw-potato/static/js/2.d12f5299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1f6cd639b24d2701f0",
+    "url": "/wjw-potato/static/js/main.662bd11c.chunk.js"
+  },
+  {
+    "revision": "c49c47ba071bac82c97e",
+    "url": "/wjw-potato/static/js/runtime-main.89a041e4.js"
+  }
+]);
